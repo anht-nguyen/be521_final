2fcb70f9509c10da21a05e848b6da9678b8ad2a6
anht-nguyen <tuananh.nguyen28898@gail.com>
2022-04-16 15:33:50 -0400

    start making classes

---
+++ lib/data_preparation.py
@@ -0,0 +1,49 @@
+#Set up the notebook environment
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
+from scipy.io import loadmat, savemat
+from scipy.stats import pearsonr
+from scipy import signal as sig
+from scipy.signal import butter, lfilter, filtfilt, ellip, resample
+from scipy.interpolate import CubicSpline
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+class DP():
+    
+    
+    
+    def filter_data
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
